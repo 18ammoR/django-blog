dbdc0c4d237cd31a6685a2565c48c4a22c34514e
Romma Perez <perezromma203@gmail.com>
2026-02-03 14:36:55 +0800

    405

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# blog/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.home_view, name='home'),  # homepage
+    path('post/<slug:slug>/', views.post_detail_view, name='post_detail'),
+    path('post/new/', views.post_create_view, name='post_create'),
+    path('post/<slug:slug>/edit/', views.post_edit_view, name='post_edit'),
+    path('post/<slug:slug>/delete/', views.post_delete_view, name='post_delete'),
+]
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'blog',      # your blog app
+    'accounts',  # for authentication
 ]
+
+# Authentication redirects
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'blog:home'
+LOGOUT_REDIRECT_URL = 'blog:home'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -114,4 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static and Media files
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
